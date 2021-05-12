300e114ef089a80cbb4b89e2a42ce071ed6c4cc3
tnguyen0306 <tnguyen0306@gmail.com>
2021-05-12 18:08:18 -0500

    Beta 1.1.0 - import data and setup chart

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+// Setup chart
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+// Import data from the csv file
+d3.csv("data.csv").then(function(healthcareData) {
+    healthcareData.forEach(function(data) {
+        data.abbr = +data.abbr;
+        data.age = +data.age;
+        data.income = +data.income;
+        data.healthcare = +data.healthcare;
+        data.obesity = +data.obesity;
+        data.smokes = +data.smokes;
+    });
+
+
+
+
+}).catch(function(error) {
+    console.log(error);
+  });
